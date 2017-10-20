be347a30268ba862aa163204b536f9d301d16454
christarikclarke <christarikclarke@gmail.com>
2017-10-20 04:36:10 -0400

    get message sending

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,6 +1,22 @@
+var self = this;
 // listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(function(id, info, tab) {
-  if (tab.url.toLowerCase().indexOf('https://meet.google.com/*') > -1) {
-    chrome.pageAction.show(tab.id);
-  }
-});
+chrome.gcm.onMessage.addListener(request => {
+  self.request = request;
+  // ---- we want to find all Chrome tabs that have Google Meet open
+  chrome.tabs.query({url: 'https://meet.google.com/*'}, tabs => {
+    tabs.forEach(tab => {
+      chrome.storage.local.get("registered", function(result) {
+        // ---- callback that is invoked by the recipient of the message that we will send next.
+        // ---- we expect the recipient to give us some text present in a notification bubble.
+        const responseHandler = (response) => {
+            chrome.notifications.create(undefined, response.notification,
+              function(notificationId) {}
+            )
+        }
+        // ---- send each Meet tab a message, which is the hotkey command from manifest.json.
+        // ---- this will always be "toggle", since that's the only one we defined.
+        chrome.tabs.sendMessage(tab.id, {request: self.request}, responseHandler)
+      });
+    })
+  })
+})
---
+++ extension/js/index.js
@@ -0,0 +1,63 @@
+var self = this;
+function registerCallback(registrationId) {
+  if (chrome.runtime.lastError) {
+    // When the registration fails, handle the error and retry the
+    // registration later.
+    return;
+  }
+
+  sendRegistrationId(registrationId, function(succeed) {
+  // Once the registration token is received by your server,
+  // set the flag such that register will not be invoked
+  // next time when the app starts up.
+  if (succeed){
+    console.log("success")
+    chrome.storage.local.set({registered: true});
+    chrome.storage.local.set({connectID: self.connectID});
+  }
+
+});
+}
+
+function sendRegistrationId(registrationId, callback) {
+
+  console.log("Send Resource");
+
+  var parameters = {
+    'connectID': self.connectID,
+    'resourceToken': registrationId
+  }
+
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    url: 'https://yag92cnm0k.execute-api.us-east-1.amazonaws.com/engagevamr/resource',
+    data: JSON.stringify(parameters),
+    success: function(d) {
+      console.log(d);
+      callback(true);
+    },
+    error: function() {
+      console.log("error");
+      callback(false);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("Page Loaded")
+  $('#submit').on('click', function() {
+    self.connectID = $('#connectID').val()
+
+    chrome.storage.local.get("registered", function(result) {
+      // If already registered, bail out.
+      if (result["registered"])
+        return;
+
+      // Up to 100 senders are allowed.
+      var senderIds = ["834636331297"];
+      chrome.gcm.register(senderIds, registerCallback);
+    });
+  });
+});
