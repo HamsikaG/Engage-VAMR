2938552e7de9122191fa93a4399b45c9d3c4ac88
HamsikaG <hamsikag15@gmail.com>
2017-11-10 14:00:01 -0500

    Fixed node lambda

--- backend/lambda.js
+++ backend/lambda.js
@@ -5,43 +5,34 @@ exports.handler = (event, context, callback) => {
     var s3 = new AWS.S3();
     console.log("Start Request");
     console.log(event);
-    var params = { Bucket: 'engage-vamr2', Key: 'resource.json',  Range: 'bytes=0-1024' }
+    var params = { Bucket: 'engage-vamr2', Key: 'resource.json'}
 
         s3.getObject(params, function(err, data) {
-          if (err) console.log("error issss "+err, err.stack); // an error occurred
+          if (err) console.log("error is "+err, err.stack); // an error occurred
           else   
-            {   // successful response
+            {   
                 console.log("File exists");
+                console.log("data: "+ data.Body.toString());
                 download_path = '/tmp/resource.json';
                 const file = fs.createWriteStream(download_path);
                 request = event;
-                                console.log("req is : "+event);   
+                console.log(request.connectID+ " is req");
+                obj = JSON.parse(data.Body.toString());
+                obj[request.connectID] = request.registrationID;
 
-                // const s3Promise = s3.getObject(params).promise();
-                var data2 = {"connectID": request.connectID, "resourceToken": request.resourceToken}
-                
-                // s3Promise.then((data) => {
-                //   file.write(data.Body, console.log('calling a callback')); 
-                // }).catch((err) => {
-                //   console.log(err);
-                // });
-
+                 console.log(JSON.stringify(obj));
                 var newData = {
                     Bucket: 'engage-vamr2',
                     Key: 'resource.json',
-                    Body : JSON.stringify(data2)
+                    Body : JSON.stringify(obj)
                 }
                 s3.putObject(newData, function(err, data) {
-                  if (err) console.log(err, err.stack); // an error occurred
+                  if (err) console.log(err, err.stack); 
                   else    {
-                       console.log(newData+ " new data"); 
-                       console.log(data);
+                      console.log(newData+ " new data"); 
+                      console.log(data);
                   }
-                               // successful response
                 });
-
-                
-               
             }         
         });
     callback(null, 'Hello from Lambda');
